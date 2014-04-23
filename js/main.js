130b35dcd08ac4109facbc33790109eb916c52c0
dippe <dippede@gmail.com>
2014-04-23 20:58:37 +0200

    comments

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
+// mivel ez a sor legkivul van, a this=window objektumnak a resze.
+// Mindig ugyelni kell, minel kevesebb kodresz legyen a window-ban, mert nagyon veszelyes:
+// - konnyen feltorheto a kod (pl. csalas celjabol)
+// - nev utkozes lehet, igy a kodok egymast irjak felul.
+
+// ez a sor blokkolja a kod tovabbi vegrehajtasat, ovatosan banni az ilyenekkel! debuggerben meg tudod nezni
 var userChoice = prompt("Do you choose rock, paper or scissors?");
 
+
+// function computerChoiceFunction(){
+// ==
+// var computerChoiceFunction = function () {
+//
+// A 2. megoldast altalaban akkor hasznaljuk, ha ki akarjuk fejezni hogy valtozoba akarjuk rakni a fuggvenyt.
+// egyebkent mindketto helyes
+
+// ez okes
 var computerChoiceFunction = function () {
 
     var computerChoice = Math.random();
@@ -15,6 +30,7 @@ var computerChoiceFunction = function () {
     return computerChoice;
 }
 
+// ez okes, majd szepitjuk
 var compare = function (choice1, choice2) {
     if (choice1 === choice2) {
         userChoice = prompt("Tie! Choose again!");
@@ -42,6 +58,9 @@ var compare = function (choice1, choice2) {
 
 }
 
+
+// ez a kod az elso sorhoz tartozik logikailag, azaz a prompt-hoz
+// epp ezert celszeru lenne 1 fuggvenybe rakni oket, es body onload -kor elinditani.
 if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
     compare(userChoice, computerChoiceFunction())
 } else {
