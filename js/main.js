3cd970992d2342da665ee9d7c2a58e0be2874ae5
dippe <dippede@gmail.com>
2014-04-24 15:38:05 +0200

    otleteles

--- js/main.js
+++ js/main.js
@@ -1,68 +1,117 @@
-// mivel ez a sor legkivul van, a this=window objektumnak a resze.
-// Mindig ugyelni kell, minel kevesebb kodresz legyen a window-ban, mert nagyon veszelyes:
-// - konnyen feltorheto a kod (pl. csalas celjabol)
-// - nev utkozes lehet, igy a kodok egymast irjak felul.
+(function() {
 
-// ez a sor blokkolja a kod tovabbi vegrehajtasat, ovatosan banni az ilyenekkel! debuggerben meg tudod nezni
-var userChoice = prompt("Do you choose rock, paper or scissors?");
+    var conditions = [
+        {winner: "rock", over:["scissors", "paper"] },
+    ];
 
+    var numOfWins = 0;
 
-// function computerChoiceFunction(){
-// ==
-// var computerChoiceFunction = function () {
-//
-// A 2. megoldast altalaban akkor hasznaljuk, ha ki akarjuk fejezni hogy valtozoba akarjuk rakni a fuggvenyt.
-// egyebkent mindketto helyes
+    var rockWins;
+    var paperWins;
+    var scissorsWins;
 
-// ez okes
-var computerChoiceFunction = function () {
+    var game = {};
+    this.game = game;
 
-    var computerChoice = Math.random();
+    game.runGame = function () {
 
-    if (computerChoice < 0.34) {
-        computerChoice = "rock";
-    } else if (computerChoice <= 0.67) {
-        computerChoice = "paper";
-    } else {
-        computerChoice = "scissors";
+        //oneGame();
+
+        runGameWithHtmlInput();
     }
-    console.log("Computer: " + computerChoice);
-    return computerChoice;
-}
+
+    game.runGameWithHtmlInput = function(){
+
+        var inputDomElem = document.getElementById("rps_input");
+
+        var userChoice = inputDomElem.value;
+
+        var textToShow = "";
+
+//        document.querySelector("#id > .className > input");
+
+        if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
+            var x = compare(userChoice, computerChoiceFunction());
+            textToShow = x.what + " wins";
+        } else {
+            textToShow = "Only rock or paper or scissors can play!";
+        }
+
+        showResult(textToShow);
+
+    }
+
+    /**
+     * private (hidden)
+     */
+
+    game.showResult(x){
+        var resultElem = document.getElementById("rps_result");
+        resultElem.innerHTML = "<b>" + x + "</b>";
+    }
+
+    game.oneGame(){
+        var userChoice = prompt("Do you choose rock, paper or scissors?");
+
+        if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
+            compare(userChoice, computerChoiceFunction())
+        } else {
+            console.log("Only rock or paper or scissors can play!")
+        }
+    }
+
+    var computerChoiceFunction = function () {
+
+        var computerChoice = Math.random();
+
+        if (computerChoice < 0.34+0.01.rockWins) {
+            computerChoice = "rock";
+        } else if (computerChoice <= 0.67) {
+            computerChoice = "paper";
+        } else {
+            computerChoice = "scissors";
+        }
+        console.log("Computer: " + computerChoice);
+        return computerChoice;
+    }
 
 // ez okes, majd szepitjuk
-var compare = function (choice1, choice2) {
-    if (choice1 === choice2) {
-        userChoice = prompt("Tie! Choose again!");
-        compare(userChoice, computerChoiceFunction());
+    var compare = function (choice1, choice2) {
+
+        var compareData = {};
+
+        if (choice1 === choice2) {
+            userChoice = prompt("Tie! Choose again!");
+            compare(userChoice, computerChoiceFunction());
+            compareData.who = "nobody";
+
+        }
+        //hogyan lehet, hogy mindig elölről kezdje?? do while-lal
+        else if (choice1 === "rock") {
+            if (choice2 === "scissors") {
+                numOfWins ++;
+                compareData.what = "rock";
+
+            }
+
+            else {
+                numOfWins ++;
+                compareData.what = "paper";
+
+            }
+        }
+        else if (choice1 === "paper") {
+            if (choice2 === "rock") {
+                compareData.what = "paper";
+
+            }
+            else {
+                compareData.what = "scissors";
+            }
+        }
+
+        return compareData;
 
     }
-    //hogyan lehet, hogy mindig elölről kezdje?? do while-lal
-    else if (choice1 === "rock") {
-        if (choice2 === "scissors") {
-            console.log("rock wins");
-        }
 
-        else {
-            console.log("paper wins");
-        }
-    }
-    else if (choice1 === "paper") {
-        if (choice2 === "rock") {
-            console.log("paper wins");
-        }
-        else {
-            console.log("scissors wins");
-        }
-    }
-
-}
-
-
-// ez a kod az elso sorhoz tartozik logikailag, azaz a prompt-hoz
-// epp ezert celszeru lenne 1 fuggvenybe rakni oket, es body onload -kor elinditani.
-if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
-    compare(userChoice, computerChoiceFunction())
-} else {
-    console.log("Only rock or paper or scissors can play!")
-}
+})()
