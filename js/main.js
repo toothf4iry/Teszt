756f70538eb0296e694e93fd4a9eb1433e8c3d7a
toothf4iry <csogoblin@gmail.com>
2014-04-30 15:38:42 +0200

    Még nem a túl szép, de egy fokkal jobb.

--- js/main.js
+++ js/main.js
@@ -1,68 +1,66 @@
-// mivel ez a sor legkivul van, a this=window objektumnak a resze.
-// Mindig ugyelni kell, minel kevesebb kodresz legyen a window-ban, mert nagyon veszelyes:
-// - konnyen feltorheto a kod (pl. csalas celjabol)
-// - nev utkozes lehet, igy a kodok egymast irjak felul.
 
-// ez a sor blokkolja a kod tovabbi vegrehajtasat, ovatosan banni az ilyenekkel! debuggerben meg tudod nezni
-var userChoice = prompt("Do you choose rock, paper or scissors?");
+    var showText = function(x,y,z){
+    var resultElem = document.getElementById(y);
+    resultElem.innerHTML =  z + " says: " + "<b>" + x + "</b>";  }
 
-
-// function computerChoiceFunction(){
-// ==
-// var computerChoiceFunction = function () {
-//
-// A 2. megoldast altalaban akkor hasznaljuk, ha ki akarjuk fejezni hogy valtozoba akarjuk rakni a fuggvenyt.
-// egyebkent mindketto helyes
-
-// ez okes
-var computerChoiceFunction = function () {
-
-    var computerChoice = Math.random();
-
-    if (computerChoice < 0.34) {
-        computerChoice = "rock";
-    } else if (computerChoice <= 0.67) {
-        computerChoice = "paper";
-    } else {
-        computerChoice = "scissors";
-    }
-    console.log("Computer: " + computerChoice);
-    return computerChoice;
-}
-
-// ez okes, majd szepitjuk
-var compare = function (choice1, choice2) {
-    if (choice1 === choice2) {
-        userChoice = prompt("Tie! Choose again!");
-        compare(userChoice, computerChoiceFunction());
-
-    }
-    //hogyan lehet, hogy mindig elölről kezdje?? do while-lal
-    else if (choice1 === "rock") {
-        if (choice2 === "scissors") {
-            console.log("rock wins");
+    var compare = function(choice1, choice2) {
+        if (choice1 === choice2) {
+            showText("Tie! Choose again!", "gameresult", "Judge" )
         }
 
-        else {
-            console.log("paper wins");
+        else if (choice1 === "rock") {
+            if (choice2 === "scissors") {
+                showText("rock wins!", "gameresult", "Judge" );
+            }
+
+            else {
+                showText("paper wins!", "gameresult", "Judge" ) ;
+            }
         }
+        else if (choice1 === "paper") {
+            if (choice2 === "rock") {
+                showText("paper wins!", "gameresult", "Judge" ) ;
+            }
+            else {
+                showText("scissors wins!", "gameresult", "Judge" ) ;
+            }
+        }
+        else if (choice1 === "scissors") {
+            if (choice2 === "rock") {
+                showText("rock wins!", "gameresult", "Judge" ) ;
+            }
+            else {
+                showText("scissors wins!", "gameresult", "Judge" ) ;
+            }
+        }
+
     }
-    else if (choice1 === "paper") {
-        if (choice2 === "rock") {
-            console.log("paper wins");
+
+    var computerChoiceFunction = function () {
+        var computerChoice = Math.random();
+
+        if (computerChoice < 0.34) {
+            computerChoice = "rock";
+        } else if (computerChoice <= 0.67) {
+            computerChoice = "paper";
+        } else {
+            computerChoice = "scissors";
         }
-        else {
-            console.log("scissors wins");
-        }
+        showText ( computerChoice, "computersays", "computer") ;
+        return computerChoice   ;
     }
 
-}
+    var runGameWithHtmlInput = function () {
+
+        var inputHtml = document.getElementById("1")
+        var userChoice = inputHtml.value
+
+        showText(userChoice, "usersays", "user")      ;
+        computerChoiceFunction()
+        if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
+            compare(userChoice, computerChoiceFunction())  }
+            else {
+            showText("Only rock, paper and scissors can play!", "gameresult", "Judge")}
+        }
 
 
-// ez a kod az elso sorhoz tartozik logikailag, azaz a prompt-hoz
-// epp ezert celszeru lenne 1 fuggvenybe rakni oket, es body onload -kor elinditani.
-if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
-    compare(userChoice, computerChoiceFunction())
-} else {
-    console.log("Only rock or paper or scissors can play!")
-}
