a6d462b8e8b2c40c0b7f3d4c706dd544c9f89424
toothf4iry <csogoblin@gmail.com>
2014-05-14 23:17:54 +0200

    Github io commit fix computersays

--- js/main.js
+++ js/main.js
@@ -1,99 +1,101 @@
-
-    var showText = function(x,y,z){
+var showText = function (x, y, z) {
     var resultElem = document.getElementById(y);
-    resultElem.innerHTML =  z + " says: " + "<b>" + x + "</b>";  }
+    resultElem.innerHTML = z + " says: " + "<b>" + x + "</b>";
+}
 
 
-    var showWhatWinsFunction = function (zs){
-        showText(zs + " wins", "gameresult", "Judge");
-    }
-    var showWhoWinsFunction = function (q){
-        showText(q + " wins", "whowonresult", "Judge")
-    }
+var showWhatWinsFunction = function (zs) {
+    showText(zs + " wins", "gameresult", "Judge");
+}
+var showWhoWinsFunction = function (q) {
+    showText(q + " wins", "whowonresult", "Judge")
+}
 
-    var compare = function(choice1, choice2) {
-         var winner = {}  ;
-        // hogy lehetne a winner-t kivezetni innen? return?
+var compare = function (choice1, choice2) {
+    var winner = {};
+    // hogy lehetne a winner-t kivezetni innen? return?
 
-        if (choice1 === choice2) {
-            showText("Tie! Choose again!", "gameresult", "Judge" )
-        }
-
-        else if (choice1 === "rock") {
-            if (choice2 === "scissors") {
-                winner.what="rock";
-                winner.who="user"
-                showWhatWinsFunction(winner.what)
-                showWhoWinsFunction(winner.who)
-            }
-
-            else {
-                winner.what = "paper";
-                winner.who = "computer"
-                showWhatWinsFunction(winner.what)
-                showWhoWinsFunction(winner.who)
-            }
-        }
-        else if (choice1 === "paper") {
-            if (choice2 === "rock") {
-                winner.what="paper";
-                winner.who = "user"
-                showWhatWinsFunction(winner.what)
-                showWhoWinsFunction(winner.who)
-            }
-            else {
-               winner.what="scissors";
-                winner.who = "computer"
-                showWhatWinsFunction(winner.what)
-                showWhoWinsFunction(winner.who)
-            }
-        }
-        else if (choice1 === "scissors") {
-            if (choice2 === "rock") {
-               winner.what="rock" ;
-                winner.who= "computer"
-                showWhatWinsFunction(winner.what)
-                showWhoWinsFunction(winner.who)
-            }
-            else {
-                winner.what="scissors";
-                winner.who = "user"
-                showWhatWinsFunction(winner.what)
-                showWhoWinsFunction(winner.who)
-
-            }
-        }
+    if (choice1 === choice2) {
+        showText("Tie! Choose again!", "gameresult", "Judge")
     }
 
-    var computerChoiceFunction = function () {
-        var computerChoice = Math.random();
-
-        if (computerChoice < 0.34) {
-            computerChoice = "rock";
-        } else if (computerChoice <= 0.67) {
-            computerChoice = "paper";
-        } else {
-            computerChoice = "scissors";
+    else if (choice1 === "rock") {
+        if (choice2 === "scissors") {
+            winner.what = "rock";
+            winner.who = "user"
+            showWhatWinsFunction(winner.what)
+            showWhoWinsFunction(winner.who)
         }
-        showText ( computerChoice, "computersays", "computer") ;
-        return computerChoice   ;
+
+        else {
+            winner.what = "paper";
+            winner.who = "computer"
+            showWhatWinsFunction(winner.what)
+            showWhoWinsFunction(winner.who)
+        }
     }
-
-    var runGameWithHtmlInput = function () {
-
-        var inputHtml = document.getElementById("1")
-        var userChoice = inputHtml.value
-
-        showText(userChoice, "usersays", "user")      ;
-
-        if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
-            computerChoiceFunction();
-            compare(userChoice, computerChoiceFunction())  }
-            else {
-            showText("Only rock, paper and scissors can play!", "gameresult", "Judge")
-            showText(" ", "computersays", "computer")                                 }
+    else if (choice1 === "paper") {
+        if (choice2 === "rock") {
+            winner.what = "paper";
+            winner.who = "user"
+            showWhatWinsFunction(winner.what)
+            showWhoWinsFunction(winner.who)
         }
+        else {
+            winner.what = "scissors";
+            winner.who = "computer"
+            showWhatWinsFunction(winner.what)
+            showWhoWinsFunction(winner.who)
+        }
+    }
+    else if (choice1 === "scissors") {
+        if (choice2 === "rock") {
+            winner.what = "rock";
+            winner.who = "computer"
+            showWhatWinsFunction(winner.what)
+            showWhoWinsFunction(winner.who)
+        }
+        else {
+            winner.what = "scissors";
+            winner.who = "user"
+            showWhatWinsFunction(winner.what)
+            showWhoWinsFunction(winner.who)
 
-    //jhgjhg
+        }
+    }
+}
+
+var computerChoiceFunction = function () {
+    var computerChoice = Math.random();
+
+    if (computerChoice < 0.34) {
+        computerChoice = "rock";
+    } else if (computerChoice <= 0.67) {
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors";
+    }
+    showText(computerChoice, "computersays", "computer");
+    return computerChoice;
+}
+
+var runGameWithHtmlInput = function () {
+
+    var inputHtml = document.getElementById("1")
+    var userChoice = inputHtml.value
+
+    showText(userChoice, "usersays", "user");
+
+    if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
+
+        compare(userChoice, computerChoiceFunction())
+    }
+    else {
+        showText("Only rock, paper and scissors can play!", "gameresult", "Judge")
+        showText(" ", "computersays", "computer")
+    }
+}
+
+//jhgjhg
 
 
